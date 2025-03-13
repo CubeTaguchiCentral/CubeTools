2c4a54eef94e96a2a67a055b0fb9226abd8a6be1
Wiz <the_wizard_who_says_ni@hotmail.com>
2025-03-13 21:40:56 +0100

    Changed YM op level setting for slot algos, as required for Galaxy Force II

--- src/com/sega/md/snd/convert/cubetofurnace/C2FYmInstrumentConverter.java
+++ src/com/sega/md/snd/convert/cubetofurnace/C2FYmInstrumentConverter.java
@@ -52,7 +52,7 @@ public class C2FYmInstrumentConverter {
         data[2] = 0;
         data[3] = 0x20;
         data[4+0*8+0] = cubeFmInstrument[0*4+0];
-        data[4+0*8+1] = ((ALGO_SLOTS[algo]&0b0001)>0)?0:cubeFmInstrument[1*4+0];
+        data[4+0*8+1] = ((ALGO_SLOTS[algo]&0b0001)>0)?(byte)(0x7F-(cubeFmInstrument[1*4+0]&0x7F)):cubeFmInstrument[1*4+0];
         data[4+0*8+2] = cubeFmInstrument[2*4+0];
         data[4+0*8+3] = cubeFmInstrument[3*4+0];
         data[4+0*8+4] = (byte)(cubeFmInstrument[4*4+0]+0x40);
@@ -62,7 +62,7 @@ public class C2FYmInstrumentConverter {
         }
         data[4+0*8+7] = 0;
         data[4+1*8+0] = cubeFmInstrument[0*4+1];
-        data[4+1*8+1] = ((ALGO_SLOTS[algo]&0b0010)>0)?0:cubeFmInstrument[1*4+1];
+        data[4+1*8+1] = ((ALGO_SLOTS[algo]&0b0010)>0)?(byte)(0x7F-(cubeFmInstrument[1*4+1]&0x7F)):cubeFmInstrument[1*4+1];
         data[4+1*8+2] = cubeFmInstrument[2*4+1];
         data[4+1*8+3] = cubeFmInstrument[3*4+1];
         data[4+1*8+4] = (byte)(cubeFmInstrument[4*4+1]+0x40);
@@ -72,7 +72,7 @@ public class C2FYmInstrumentConverter {
         }
         data[4+1*8+7] = 0;
         data[4+2*8+0] = cubeFmInstrument[0*4+2];
-        data[4+2*8+1] = ((ALGO_SLOTS[algo]&0b0100)>0)?0:cubeFmInstrument[1*4+2];
+        data[4+2*8+1] = ((ALGO_SLOTS[algo]&0b0100)>0)?(byte)(0x7F-(cubeFmInstrument[1*4+2]&0x7F)):cubeFmInstrument[1*4+2];
         data[4+2*8+2] = cubeFmInstrument[2*4+2];
         data[4+2*8+3] = cubeFmInstrument[3*4+2];
         data[4+2*8+4] = (byte)(cubeFmInstrument[4*4+2]+0x40);
@@ -82,7 +82,7 @@ public class C2FYmInstrumentConverter {
         }
         data[4+2*8+7] = 0;
         data[4+3*8+0] = cubeFmInstrument[0*4+3];
-        data[4+3*8+1] = ((ALGO_SLOTS[algo]&0b1000)>0)?0:cubeFmInstrument[1*4+3];
+        data[4+3*8+1] = ((ALGO_SLOTS[algo]&0b1000)>0)?(byte)(0x7F-(cubeFmInstrument[1*4+3]&0x7F)):cubeFmInstrument[1*4+3];
         data[4+3*8+2] = cubeFmInstrument[2*4+3];
         data[4+3*8+3] = cubeFmInstrument[3*4+3];
         data[4+3*8+4] = (byte)(cubeFmInstrument[4*4+3]+0x40);
